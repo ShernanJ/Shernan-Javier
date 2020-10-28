d694fd2c79b58aa8b8de993606412b796fcfa57e
Shernan Javier <shernanjavier@gmail.com>
2020-10-27 22:14:07 -0400

    Created Navigation Component

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from 'styled-components';
+
+import Logo from './Logo';
+import { NavItems, Link, Resume} from "./NavigationStyles";
+
+const Wrapper = styled.div`
+  display: flex;
+  height:100%;
+  /* box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.5); */
+  flex-flow: row;
+  padding: 16px 0px;
+`;
+
+const Flex = styled.div`
+  flex: 50%;
+`;
+
+const Navigation = (props) => {
+  return (
+    <Wrapper>
+      <Logo />
+      <Flex><NavItems>
+        <Link href="/">About</Link>
+        <Link href="/">Projects</Link>
+        <Link href="/">Contact</Link>
+        <Resume>Resume</Resume>
+      </NavItems></Flex>
+    </Wrapper>
+  );
+};
+
+export default Navigation;
