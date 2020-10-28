d5019aaf30aefaa0c209ad5fd35894b81814f572
Shernan Javier <shernanjavier@gmail.com>
2020-10-28 00:27:32 -0400

    Fixed whitespace issue

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Layout from '../Components/Layout';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  margin: 0;
+  padding: 0;
+`;
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <Layout />
-    </div>
+    </Wrapper>
   );
 }
 
