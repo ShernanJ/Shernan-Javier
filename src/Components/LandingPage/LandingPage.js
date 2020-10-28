47f08e3212dd27db4d3b19acf08eb99c41d52dda
Shernan Javier <shernanjavier@gmail.com>
2020-10-27 22:12:49 -0400

    Initialized Landing Page Component

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`;
+
+const LandingPage = (props) => {
+
+  return(
+    <Wrapper>
+      <h1>Hello</h1>
+    </Wrapper>
+  )
+}
+
+export default LandingPage;
