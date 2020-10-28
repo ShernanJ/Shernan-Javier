f6a35ec233e68f2b282319f0bbf7e249af7e8610
Shernan Javier <shernanjavier@gmail.com>
2020-10-27 22:14:15 -0400

    Created Navigation Component

---
+++ src/Components/Navigation/NavigationStyles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const NavItems = styled.ul`
+  display: flex;
+  justify-content: flex-end;
+  flex-flow: row;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  height: 100%;
+`;
+
+export const Link = styled.a`
+  font-size: 24px;
+  font-weight: 350;
+  color: #1584CE;
+  text-decoration: none;
+  height: 50%;
+  width: auto;
+  padding: 16px 10px;
+  transition: color 0.5s ease;
+  padding: 0px;
+  margin: 16px 16px;
+  &:hover,
+  &:active {
+    color: #072336;
+    cursor: pointer;
+  }
+`;
+
+export const Resume = styled(Link)`
+  color: #fa504c;
+  border: thin #fa504c solid;
+  border-radius: 5px;
+  transition: color 0.5s, background-color 0.5s;
+  padding: 0px 10px;
+  margin-right: 32px;
+  &:hover,
+  &:active {
+    border-bottom: thin #fa504c solid;
+    color: white;
+    background-color: #fa504c;
+  }
+`;
+
+export const Logo = styled.div`
+
+`;
