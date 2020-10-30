7bbebf5ed389afe3aa66b169f3cffd0098a4e603
Shernan Javier <shernanjavier@gmail.com>
2020-10-30 02:15:48 -0400

    Fixed issues with size of about section

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 import Content from './Content/Content';
 
 const Wrapper = styled.div`
-
+  display: block;
+  height:100%;
+  width: 100%;
 `;
 
 const About = (props) => {
   return(
-    <Wrapper>
+    <Wrapper id="about">
       <Content />
     </Wrapper>
   )
