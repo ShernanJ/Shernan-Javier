6a93f7afdf211350201cbb827b845d0008b3da79
Shernan Javier <shernanjavier@gmail.com>
2020-11-02 18:17:52 -0500

    Separated styled components

--- src/Components/Pages/About/Content/Content.js
+++ src/Components/Pages/About/Content/Content.js
@@ -1,27 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import {Image, ImageWrapper, Wrapper} from './ContentStyles';
 
 import Texts from "./Texts";
 import mainImage from "../../../../assets/HandsomeMan.png";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100%;
-  width: 78.6%;
-  margin: 0 auto;
-`;
-
-const ImageWrapper = styled.div`
-  flex: auto;
-  padding-top: 22%;
-`;
-
-const Image = styled.img`
-  display: block;
-  height: auto;
-  width: 350px;
-  margin: auto;
-`;
 
 const Content = (props) => {
   return (
