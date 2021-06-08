aa19e7427fd9871964b7af4dfd8a331be1c24154
Shernan Javier <shernanjavier@gmail.com>
2021-06-08 01:28:30 -0400

    Added Modal to Layout

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import LandingPage from './Pages/LandingPage/LandingPage';
-import About from './Pages/About/About';
-import Projects from './Pages/Projects/Projects';
-import Contact from './Pages/Contact/Contact';
-import Navigation from './Navigation/Navigation';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import About from "./Pages/About/About";
+import Projects from "./Pages/Projects/Projects";
+import Contact from "./Pages/Contact/Contact";
+import Navigation from "./Navigation/Navigation";
+import styled from "styled-components";
+import Modal from "./UI/Modal/Modal";
+import Resume from "./Pages/Resume/Resume";
 
 const Wrapper = styled.div`
   height: 100%;
-  width:100%;
+  width: 100%;
 `;
 
-
 const Layout = (props) => {
-  return(
+  const [resumeVisible, toggleResume] = useState(false);
+  return (
     <Wrapper>
-      <Navigation />
+      <Modal show={resumeVisible} modalClosed={() => toggleResume(false)}>
+        <Resume />
+      </Modal>
+      <Navigation visible={() => toggleResume("block")} />
       <LandingPage />
       <About />
       <Projects />
       <Contact />
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Layout;
