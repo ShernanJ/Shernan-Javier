372e58fd8f3e8aa431090829298a3cef6c051886
Shernan Javier <shernanjavier@gmail.com>
2021-06-08 01:30:29 -0400

    Improved Nav Bar, added scroll listener feature

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,31 +1,55 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+import DrawerToggle from "./DrawerToggle/DrawerToggle";
 
-import Logo from './Logo';
-import { NavItems, Link, Resume} from "./NavigationStyles";
+import Logo from "./Logo";
+import { NavItems, Link, ResumeBtn } from "./NavigationStyles";
 
 const Wrapper = styled.div`
   position: fixed;
   display: flex;
   /* box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.5); */
   flex-flow: row;
-  padding: 16px 0px;
+  padding: 16px 2.5%;
   background-color: white;
-  width: 100%;
+  width: 95%;
   top: 0;
   z-index: 1;
+  transition: transform 0.4s;
+  @media only screen and (min-width: 769px) {
+    padding: 16px 0;
+    width: 100%;
+  }
 `;
 
-
 const Navigation = (props) => {
+  const [visible, setVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+    setPrevScrollPos(currentScrollPos);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <Wrapper>
+    <Wrapper
+      style={{ transform: visible ? "translateY(0)" : "translateY(-15vh)" }}
+    >
       <Logo />
+      <DrawerToggle />
       <NavItems>
         <Link href="#about">About</Link>
         <Link href="/">Projects</Link>
         <Link href="/">Contact</Link>
-        <Resume>Resume</Resume>
+        <ResumeBtn onClick={props.visible}>Resume</ResumeBtn>
       </NavItems>
     </Wrapper>
   );
