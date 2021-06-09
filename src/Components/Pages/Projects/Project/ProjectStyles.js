84eeae290583da16062bf6af7d4c0a1429e8dea4
Shernan Javier <shernanjavier@gmail.com>
2021-06-09 07:01:08 -0400

    Cleaned up Button

--- src/Components/Pages/Projects/Project/ProjectStyles.js
+++ src/Components/Pages/Projects/Project/ProjectStyles.js
@@ -22,3 +22,18 @@ export const TextContainer = styled.div`
   flex: auto;
   background-color: pink;
 `;
+
+export const DemoButton = styled.button`
+  font-weight: 600;
+  height: 10.78%;
+  width: 17.625%;
+  color: white;
+  background: #0163f7;
+  box-shadow: 0px 1.75305px 2.3374px rgba(0, 0, 0, 0.25);
+  border-radius: 2.03774px;
+  border: none;
+`;
+
+export const CodeButton = styled(DemoButton)`
+  background: #444452;
+`;
