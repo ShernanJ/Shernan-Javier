1fbfaf1fb9d09deaa213750470366f264ba5ae36
Shernan Javier <shernanjavier@gmail.com>
2021-06-09 06:37:54 -0400

    Added auto rendering list of projects feature

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from "react";
+import Project from "./Project/Project";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: block;
+  min-height: 100%;
+  width: 100%;
+  background-color: red;
+`;
 
 const Projects = (props) => {
-  return(
-    <div>Projects</div>
-  )
-}
+  // Insert project name here, place preview in images folder with same project name.
+  const projects = require("./projects.json");
+
+  const projectList = projects.map((project, i) => (
+    <Project
+      key={i}
+      index={i}
+      name={project.name}
+      desc={project.desc}
+      tools={project.tools}
+      demoLink={project.demo_link}
+      githubLink={project.github_link}
+      image={project.image}
+    />
+  ));
+
+  return (
+    <Wrapper>
+      <h1>Projects</h1>
+      {projectList}
+    </Wrapper>
+  );
+};
 export default Projects;
