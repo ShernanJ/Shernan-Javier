56500001b9b1b5ab8f2f56acfc59d5cb3eed87de
Shernan Javier <shernanjavier@gmail.com>
2021-06-09 07:17:54 -0400

    Removed random background colors and added button hover styling

--- src/Components/Pages/Projects/Project/ProjectStyles.js
+++ src/Components/Pages/Projects/Project/ProjectStyles.js
@@ -4,7 +4,6 @@ export const Wrapper = styled.div`
   display: flex;
   width: 78.6%;
   margin: auto;
-  background-color: blue;
   flex-direction: row;
   padding-bottom: 5%;
 `;
@@ -20,20 +19,39 @@ export const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: auto;
-  background-color: pink;
 `;
 
 export const DemoButton = styled.button`
   font-weight: 600;
-  height: 10.78%;
   width: 17.625%;
+  flex: 1;
   color: white;
   background: #0163f7;
   box-shadow: 0px 1.75305px 2.3374px rgba(0, 0, 0, 0.25);
   border-radius: 2.03774px;
   border: none;
+  margin-right: 5%;
+  transition: all 0.4s;
+  &:hover {
+    cursor: pointer;
+    background: #094cb3;
+  }
 `;
 
 export const CodeButton = styled(DemoButton)`
   background: #444452;
+
+  &:hover {
+    background: #2a2a33;
+  }
+`;
+
+export const Spacing = styled.div`
+  flex: 0 0 6%;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  height: 10.78%;
+  width: 50%;
 `;
