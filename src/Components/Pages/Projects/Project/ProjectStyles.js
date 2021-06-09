2a41de9b80da49cf7c8bb48cb78e48e0dd0dcadd
Shernan Javier <shernanjavier@gmail.com>
2021-06-09 06:39:05 -0400

    Styled project component

---
+++ src/Components/Pages/Projects/Project/ProjectStyles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 78.6%;
+  margin: auto;
+  background-color: blue;
+  flex-direction: row;
+  padding-bottom: 5%;
+`;
+
+export const Image = styled.img`
+  flex: 0 0 52.083%;
+  width: 47.917%;
+  height: auto;
+  box-shadow: 0px 2px 4px 1px rgba(0, 0, 0, 0.25);
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: auto;
+  background-color: pink;
+`;
