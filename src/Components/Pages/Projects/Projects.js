527912eeea520b6a4bbca3de94320ac47eb58e82
Shernan Javier <shernanjavier@gmail.com>
2021-06-09 11:47:33 -0400

    Added responsiveness

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -32,6 +32,12 @@ const Wrapper = styled.div`
   display: block;
   min-height: 100%;
   width: 100%;
+
+  margin-top: 30%;
+
+  @media only screen and (min-width: 769px) {
+    margin-top: 0%;
+  }
 `;
 
 const Title = styled.h1`
