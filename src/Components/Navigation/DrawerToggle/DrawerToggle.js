db214234d3301722ab5f97ab5e03d0e75b5478e5
Shernan Javier <shernanjavier@gmail.com>
2021-06-16 23:35:20 -0400

    Added side drawer

--- src/Components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/Components/Navigation/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import SideDrawer from "../SideDrawer/SideDrawer";
 
 const Wrapper = styled.div`
   display: flex;
@@ -7,6 +8,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: flex-end;
   padding-right: 8px;
+  z-index: 99;
   @media only screen and (min-width: 769px) {
     display: none;
   }
@@ -21,49 +23,66 @@ const Menu = styled.div`
   margin: auto 0;
   width: 25px;
   cursor: pointer;
-
-
+  z-index: 100;
 `;
 
 const Bar = styled.div`
   position: relative;
   height: 3px;
-  background-color: #1584CE;
+  background-color: #1584ce;
   /* transition: 0.6s all; */
   width: 100%;
 `;
 
 const DrawerToggle = (props) => {
-
-  let opened = false;
-
-
+  const [isDrawerOpened, toggleDrawerOpened] = useState(false);
   const handleClick = () => {
     let midBar = document.getElementById("mid-bar");
     let topBar = document.getElementById("top-bar");
     let botBar = document.getElementById("bot-bar");
-    
-    if(opened) {
-      midBar.setAttribute('style', "transition-duration: 0.22s; transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);");
-      topBar.setAttribute('style', "top: 0; transition: top 0.1s 0.25s ease-in, opacity 0.1s ease-in;");
-      botBar.setAttribute('style', "bottom: 0; transition: bottom 0.1s 0.25s ease-in, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19);");
-    } else{
-      midBar.setAttribute('style', "transform: rotate(-230deg); transition-delay: 0.12s; transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1)");
-      topBar.setAttribute('style', "top: 8px; opacity: 0; transition: top 0.1s ease-out, opacity 0.1s 0.12s ease-out;");
-      botBar.setAttribute('style', "bottom: 8px; transform: rotate(230deg); transition: bottom 0.1s ease-out, transform 0.22s 0.12s cubic-bezier(0.215, 0.61, 0.355, 1);");
-    }
-    opened = !opened;
-  }
 
+    if (isDrawerOpened) {
+      midBar.setAttribute(
+        "style",
+        "transition-duration: 0.22s; transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);"
+      );
+      topBar.setAttribute(
+        "style",
+        "top: 0; transition: top 0.1s 0.25s ease-in, opacity 0.1s ease-in;"
+      );
+      botBar.setAttribute(
+        "style",
+        "bottom: 0; transition: bottom 0.1s 0.25s ease-in, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19);"
+      );
+    } else {
+      midBar.setAttribute(
+        "style",
+        "transform: rotate(-230deg); transition-delay: 0.12s; transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1)"
+      );
+      topBar.setAttribute(
+        "style",
+        "top: 8px; opacity: 0; transition: top 0.1s ease-out, opacity 0.1s 0.12s ease-out;"
+      );
+      botBar.setAttribute(
+        "style",
+        "bottom: 8px; transform: rotate(230deg); transition: bottom 0.1s ease-out, transform 0.22s 0.12s cubic-bezier(0.215, 0.61, 0.355, 1);"
+      );
+    }
+    toggleDrawerOpened(!isDrawerOpened);
+    console.log("Clicked");
+  };
   return (
-    <Wrapper>
-      <Menu onClick={() => handleClick()} >
+    <Wrapper scroll={isDrawerOpened}>
+      <Menu onClick={() => handleClick()}>
         <Bar id="top-bar" />
         <Bar id="mid-bar" />
         <Bar id="bot-bar" />
       </Menu>
+      <SideDrawer
+        style={isDrawerOpened ? "translateX(0)" : "translate(100vw)"}
+      />
     </Wrapper>
-  )
-}
+  );
+};
 
 export default DrawerToggle;
