c3677fb8adb58d78b0b5f16d76f55886a540b7d0
Shernan Javier <shernanjavier@gmail.com>
2021-06-16 23:35:51 -0400

    Fixed responsiveness

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import Content from './Content/Content';
+import Content from "./Content/Content";
 
 const Wrapper = styled.div`
   display: block;
   min-height: 100%;
   width: 100%;
+  margin-top: 50%;
+
+  @media only screen and (min-width: 769px) {
+    margin-top: 0%;
+  }
 `;
 
 const About = (props) => {
-  return(
+  return (
     <Wrapper id="about">
       <Content />
     </Wrapper>
-  )
-}
+  );
+};
 export default About;
