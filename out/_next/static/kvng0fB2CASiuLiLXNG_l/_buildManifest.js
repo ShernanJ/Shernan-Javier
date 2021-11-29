968abb86d809cf70d4d05a2ece4da36ce6a10581
Shernan Javier <shernanjavier@gmail.com>
2021-11-29 18:07:39 -0500

    edited gitignore

--- out/_next/static/kvng0fB2CASiuLiLXNG_l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-88e026a894747fc5.js","static/chunks/pages/index-3586aa48443a298d.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
